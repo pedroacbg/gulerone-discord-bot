4383b8aa4f03b372d2ffd8a85a6b04ce105150cb
Pedro Augusto Costa Anjos <pedrooaugustoo130@gmail.com>
2023-04-02 16:28:33 -0300

    more anime content

---
+++ commands/anime-nsfw.js
@@ -0,0 +1,37 @@
+const { SlashCommandBuilder, EmbedBuilder } = require("discord.js");
+
+const exampleEmbed = new EmbedBuilder()
+  .setColor(0x9d4383)
+  .setTitle("Coisas de anime para alegrar seu dia")
+  .setDescription("Você irá ver algo incrível")
+  .setTimestamp()
+  .setFooter({
+    text: "Eu sei que você gosta",
+    iconURL:
+      "https://static.wikia.nocookie.net/my-brother-is-done-for/images/d/d0/MahiroEX.jpg/revision/latest/scale-to-width-down/300?cb=20210226184246",
+  });
+
+const categories = ["waifu", "neko", "trap", "blowjob"];
+
+let randomPercent = Math.floor(Math.random() * categories.length);
+let randomCategory = categories[randomPercent];
+
+const animeImage = function () {
+  fetch(`https://api.waifu.pics/nsfw/${randomCategory}`)
+    .then((response) => response.json())
+    .then((data) => {
+      exampleEmbed.setImage(data.url);
+    });
+};
+animeImage();
+
+module.exports = {
+  data: new SlashCommandBuilder()
+    .setName("anime-nsfw")
+    .setDescription("Responde com algumas fotos"),
+
+  async execute(interaction) {
+    animeImage();
+    await interaction.reply({ embeds: [exampleEmbed] });
+  },
+};
